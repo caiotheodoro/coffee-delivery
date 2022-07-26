e12a364dba1359c2f590cd9b69e969531f2c5411
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-07-26 01:14:03 -0300

    checkout screen finised

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,9 +1,20 @@
-import { Bank, CreditCard, CurrencyDollar, MapPinLine, Money } from "phosphor-react";
+import { Bank, CreditCard, CurrencyDollar, MapPinLine, Minus, Money, Plus, Trash } from "phosphor-react";
+import { useState } from "react";
 import { HeadCheckout } from "./components/Head";
 import { InputCheckout } from "./components/Input";
-import { ButtonList,OptionButton, CheckoutContainer, InfoContainer, InputPanel, OrderContainer } from "./styles";
+import { ButtonList, OptionButton, CheckoutContainer, InfoContainer, InputPanel, OrderContainer, CounterButton, RemoveButton, ItemContainer, PricingContainer, ConfirmPaymentButton } from "./styles";
 
 export function Checkout() {
+  const [counter, setCounter] = useState(0);
+
+
+  const handleAdd = () => {
+    setCounter((state) => state + 1)
+  }
+
+  const handleRemove = () => {
+    counter > 0 && setCounter((state) => state - 1);
+  }
 
   const handleChoosePayment = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -17,28 +28,28 @@ export function Checkout() {
         <h1>Complete seu pedido</h1>
         <form>
           <article>
-            <HeadCheckout 
+            <HeadCheckout
               iconColor="yellowDark"
               title=" Endereço de entrega"
               subtitle="Informe o endereço onde deseja receber seu pedido"
               icon={<MapPinLine size={22} />}
             />
             <InputPanel>
-              <InputCheckout type="number" placeholder="CEP" widthInput="12.5rem"  />
+              <InputCheckout type="number" placeholder="CEP" widthInput="12.5rem" />
               <InputCheckout type="text" placeholder="Rua" widthInput="100%" />
               <div>
-                <InputCheckout type="number" placeholder="Número" widthInput="12.5rem"  />
-                <InputCheckout type="text" placeholder="Complemento"  widthInput="100%"   />
+                <InputCheckout type="number" placeholder="Número" widthInput="12.5rem" />
+                <InputCheckout type="text" placeholder="Complemento" widthInput="100%" />
               </div>
               <div>
-                <InputCheckout type="text" placeholder="Bairro" widthInput="12.5rem"  />
-                <InputCheckout type="text" placeholder="Cidade" widthInput="100%"  />
+                <InputCheckout type="text" placeholder="Bairro" widthInput="12.5rem" />
+                <InputCheckout type="text" placeholder="Cidade" widthInput="100%" />
                 <InputCheckout type="text" placeholder="UF" widthInput={'3.75rem'} />
               </div>
             </InputPanel>
           </article>
           <article>
-          <HeadCheckout 
+            <HeadCheckout
               iconColor="purple"
               title="Pagamento"
               subtitle="O pagamento é feito na entrega. Escolha a forma que deseja pagar"
@@ -47,15 +58,15 @@ export function Checkout() {
 
             <ButtonList>
               <OptionButton onClick={handleChoosePayment}>
-                <CreditCard size={16}  />
+                <CreditCard size={16} />
                 <h1>CARTÃO DE CRÉDITO</h1>
               </OptionButton>
               <OptionButton onClick={handleChoosePayment}>
-                <Bank size={16}  />
+                <Bank size={16} />
                 <h1>CARTÃO DE DÉBITO</h1>
               </OptionButton>
               <OptionButton onClick={handleChoosePayment}>
-                <Money size={16}  />
+                <Money size={16} />
                 <h1>DINHEIRO</h1>
               </OptionButton>
             </ButtonList>
@@ -65,8 +76,47 @@ export function Checkout() {
       <OrderContainer>
         <h1>Cafés selecionados</h1>
         <article>
-
+          <ItemContainer>
+            <img src={`src/assets/coffees/Type=Árabe.png`} alt="" />
+            <div>
+              <h1>Expresso Tradicional</h1>
+              <span>
+                <CounterButton><Minus size={14} weight="fill" onClick={handleRemove} /> {counter} <Plus size={14} weight="fill" onClick={handleAdd} /></CounterButton>
+                <RemoveButton><Trash size={16} />REMOVER</RemoveButton>
+              </span>
+            </div>
+            <h2>R$ 9,90</h2>
+          </ItemContainer>
+          <ItemContainer>
+            <img src={`src/assets/coffees/Type=Árabe.png`} alt="" />
+            <div>
+              <h1>Expresso Tradicional</h1>
+              <span>
+                <CounterButton><Minus size={14} weight="fill" onClick={handleRemove} /> {counter} <Plus size={14} weight="fill" onClick={handleAdd} /></CounterButton>
+                <RemoveButton><Trash size={16} />REMOVER</RemoveButton>
+              </span>
+            </div>
+            <h2>R$ 9,90</h2>
+          </ItemContainer>
+          <PricingContainer>
+            <div>
+              <span>
+                <h2>Total de itens</h2>
+                <h2>R$ 9,90</h2>
+              </span>
+              <span>
+                <h2>Entrega</h2>
+                <h2>R$ 3,50</h2>
+              </span>
+              <span>
+                <h1>Total</h1>
+                <h1>R$ 12,40</h1>
+              </span>
+            </div>
+            <ConfirmPaymentButton>CONFIRMAR PEDIDO</ConfirmPaymentButton>
+          </PricingContainer>
         </article>
+        
       </OrderContainer>
     </CheckoutContainer>
   )
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -16,13 +16,17 @@ const BaseContainer = styled.div`
     flex-direction: column;
     justify-content: flex-start;
 
-   
 
     h1:first-child {
-        font-family: "Baloo 2";
+        margin-bottom: 1rem;
+
+    }
+   
+
+    font-family: "Baloo 2";
+    h1 {
         font-weight: 700;
         font-size: 1.125rem;
-        margin-bottom: 1rem;
     }
     
     article {
@@ -53,9 +57,54 @@ export const OrderContainer = styled(BaseContainer)`
 
     article {
         border-radius: 6px 44px;
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        justify-content: center;
+
+        img {
+            width: 4rem;
+            height: 4rem;
+        }
+        
+         
+
     }
 
+    
+    
 `
+export const ItemContainer = styled.div`
+
+        display: flex;
+        gap: 1.25rem;
+        flex-direction: row;
+        justify-content: space-between;
+
+        h1 {
+            font-family: "Roboto";
+            font-weight: 400;
+            font-size: 1rem;
+        }
+
+        h2 {
+            font-family: "Roboto";
+            font-weight: 700;
+            font-size: 0.875rem;
+        }
+
+        span {
+            display: flex;
+            gap: 1rem;
+        }
+        
+        border-bottom: 1px solid #E6E5E5;
+        border-radius: 6px 0px 0px 6px;
+        padding-bottom: 2rem;
+        margin-bottom: 2rem;
+
+`
+
 
 export const InputPanel = styled.div`
     display: flex;
@@ -86,8 +135,6 @@ const BaseButton = styled.button`
         font-weight: 500;
         cursor: pointer;
         gap: 0.75rem;
-        padding: 1rem;
-        width: 100%;
         
         h1 {
             font-family: "Roboto";
@@ -109,6 +156,8 @@ const BaseButton = styled.button`
 export const OptionButton = styled(BaseButton)`
     background-color: ${({ theme }) => theme['base-button']};
     color: ${({ theme }) => theme['base-text']};
+    padding: 1rem;
+    width: 100%;
 
     svg {
         color: ${({ theme }) => theme['purple']};
@@ -124,4 +173,71 @@ export const ButtonList = styled.div`
     gap: 0.75rem;
     width: 100%;
 
+`
+
+export const CounterButton = styled(BaseButton)`
+  background-color: ${({ theme }) => theme['base-button']};
+  font-size: 0.875rem;
+  padding: 8px;
+  svg {
+    color: ${({ theme }) => theme['purple']};
+
+    
+    :hover {
+        color: ${({ theme }) => theme['purple-dark']};
+    }
+}
+
+  `
+
+
+export const RemoveButton = styled(BaseButton)`
+    background-color: ${({ theme }) => theme['base-button']};
+    color: ${({ theme }) => theme['base-text']};
+    padding: 0px 8px;
+    width: 100%;
+    font-size: 0.625rem;
+    svg {
+        color: ${({ theme }) => theme['purple']};
+    }
+    
+`
+
+export const PricingContainer = styled.div`
+display: flex;
+flex-direction: column;
+
+div {
+    display: flex;
+    flex-direction: column;
+    gap: 0.75rem;
+
+    span {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+    }
+    h2 {
+        font-family: "Roboto";
+        font-weight: 400;
+        font-size: 0.875rem;
+    }
+
+    h1 {
+        font-size: 1.25rem;
+        font-family: "Roboto";
+        font-weight: 700;
+    }
+}
+`
+
+export const ConfirmPaymentButton = styled(BaseButton)`
+    background-color: ${({ theme }) => theme['yellow']};
+    color: ${({ theme }) => theme['white']};
+    padding: 12px 8px;
+    justify-content: center;
+    width: 100%;
+    font-size: 0.875rem;
+  
+    
 `
